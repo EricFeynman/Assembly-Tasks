bb4119033d8e1d72da2c807b4fa946ee8c392d3b
Eric <147992178+EricFeynman@users.noreply.github.com>
2024-02-27 15:16:35 +0600

    Update 3.24.asm

--- Contest/2_week/3.24.asm
+++ Contest/2_week/3.24.asm
@@ -1,40 +1,62 @@
-%include 'io64.inc'
+%include "io64.inc"
+extern printf
+section .data
+section .bss
+    msg resb 100 
 section .text
 global main
 main:
-    mov rbp, rsp; for correct debugging
-    ;write your code here
-    xor rax, rax    ;char
-    xor rbx, rbx    ;flag
-    PRINT_CHAR '('
-    
+    mov rbp, rsp; for correct debugging   
+    xor rcx, rcx  ;length
+    mov rax, msg    ;string
+    xor rbx, rbx    ;char
+    xor rdx, rdx    ;num
 loop1:
-    GET_CHAR rax
-    cmp rax, '.'
-    je exit
-    cmp rax, 10
-    je next_line
-    cmp rax, ' '
-    je space
-    PRINT_CHAR rax
-    xor rbx, rbx
+    GET_CHAR rbx 
+    mov [rax + rcx], rbx
+    inc rcx
+    cmp rbx, '.'
+    je to_func
     jmp loop1
     
-space:
-    cmp rbx, 0
-    jne loop1
-    PRINT_CHAR ')'
-    PRINT_CHAR ' '
-    PRINT_CHAR '('
-    inc rbx
-    jmp loop1
-
-next_line:
-    PRINT_CHAR ')'
-    NEWLINE
-    PRINT_CHAR '('
-    jmp loop1               
+to_func:
+    push rax
+    push rcx
+    call str_to_num
+    add rsp, 16
     
-exit:    
-    PRINT_CHAR ')'   
+    PRINT_DEC 8, rdx
     ret
+    
+;-----------------//CONVERT STRING TO NUMBER//-----------------
+str_to_num:
+push rbp
+mov rbp, rsp
+    push rsi    ;char
+    push rdi    ;index
+    xor rdi, rdi
+loop2:
+    cmp rdi, rcx
+    jnb str_to_num_exit
+    mov rsi, qword[rax + rdi]
+    cmp rsi, '0'
+    jbe to_next
+    cmp rax, '9'
+    ja to_next
+    sub rsi, '0'
+    imul rdx, rdi
+    add rax, rsi
+    inc rdi   
+    jmp loop2
+    
+to_next:
+    inc rdi
+    jmp loop2
+    
+str_to_num_exit:
+    pop rdi
+    pop rsi
+    ;mov rdx, rax
+    leave
+    ret
+    
