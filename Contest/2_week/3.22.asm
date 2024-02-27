2e59fde0abcd21c7e191f05be9015a7d0ee0eb6f
Eric <147992178+EricFeynman@users.noreply.github.com>
2024-02-27 15:15:45 +0600

    Update 3.22.asm

--- Contest/2_week/3.22.asm
+++ Contest/2_week/3.22.asm
@@ -1,40 +1,26 @@
-%include 'io64.inc'
+%include "io64.inc"
 section .text
 global main
 main:
     mov rbp, rsp; for correct debugging
     ;write your code here
-    xor rax, rax
-    xor rbx, rbx    ;cnt "("
-    xor rcx, rcx    ;cnt ")"
-    
-loop1:
+    xor rax, rax    ;char
+    xor rcx, rcx    ;cnt
+loop1: 
     GET_CHAR rax
     cmp rax, 10
-    je compare
-    cmp rax, '('
-    je openpp
-    cmp rax, ')'
-    je closepp
-    jmp loop1
-    
-openpp:
-    inc rbx
-    jmp loop1
-    
-closepp:
+    je print
+    push rax
     inc rcx
-    cmp rbx, rcx
-    jb neg_answer
     jmp loop1
-
-compare:
-    cmp rbx, rcx
-    jne neg_answer
-    PRINT_STRING 'YES'
-    jmp exit
-neg_answer:
-    PRINT_STRING 'NO'
     
-exit:
-    ret
+print:
+    cmp rcx, 0
+    je exit
+    pop rax
+    PRINT_CHAR rax
+    dec rcx
+    jmp print
+         
+exit:   
+      ret
